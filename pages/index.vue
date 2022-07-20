9e844326bedd7647fd2ee2eeff5bd87bdc50663d
kikujiro2020 <kikujiro34@gmail.com>
2022-07-21 01:54:12 +0900

    Nuxt3 no4 on YouTube

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>    
+    <div class="container-fluid">
+        <h1>Index</h1>
+        <NuxtLink to="/secondpage">to SecondPage</NuxtLink>
+    </div>
+</template>
---
+++ pages/secondpage.vue
@@ -0,0 +1,7 @@
+<template>
+     <div class="container-fluid">
+        <h1>Second Page</h1>
+        
+        <NuxtLink to="/">Home</NuxtLink>
+    </div>
+</template>
