514b65c97e7a38efdc7763b7500bb95375ae54db
kikujiro2020 <kikujiro34@gmail.com>
2022-07-30 01:32:33 +0900

    at YouTube06

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="container-fluid">
-        <Navbar />
+       
         <h1>Index</h1>
         <NuxtLink to="/secondpage">to SecondPage</NuxtLink>
     </div>
 </template>
+
+<script setup>
+// This will also work in `<script setup>`
+definePageMeta({
+  layout: "sample",
+});
+</script>
