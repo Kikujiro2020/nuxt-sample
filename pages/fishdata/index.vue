086e759413f816813a6ce34aa59a47ad345658e0
kikujiro2020 <kikujiro34@gmail.com>
2022-08-11 15:48:13 +0900

    YouTube8 composables directory & useState

--- pages/fishdata/index.vue
+++ pages/fishdata/index.vue
@@ -1,13 +1,19 @@
 <script setup>
 import data from "/data.json"
+const { counter, inc, dec } = useCounter()
+onMounted(() => { console.log(counter.value) })
+
 </script>
 <template>
     <div class="container-fluid">
 
+        <p>{{ counter }}</p>
+        <button class="btn" @click="inc"> + </button>
+        <button class="btn" @click="dec"> - </button>
         <h1>Fish data</h1>
         <div v-for="value in data">
-                    
-            <NuxtLink :to='"/fishdata/"+value.id'>{{ value.name }}</NuxtLink>
+            <NuxtLink :to='"/fishdata/" + value.id'>{{ value.name }}</NuxtLink>
+
         </div>
     </div>
 </template>
