a3cb76053488eccd6209069322971d5c1b9d3b50
kikujiro2020 <kikujiro34@gmail.com>
2022-09-22 01:51:08 +0900

    yarn.lock file was changed. because I should upgrade some packages.

---
+++ pages/thirdpage.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { counter, inc, dec } = useCounter()
+
+</script>
+
+<template>
+    <div class="container-fluid">
+
+        <h1>Composables sample</h1>
+        <p>{{ counter }}</p>
+        <button type="button" class="btn btn-outline-primary" @click="inc"> + </button>
+        <button type="button" class="btn btn-outline-secondary" @click="dec"> - </button>
+        <NuxtLink to="/">Home</NuxtLink>
+    </div>
+</template>
